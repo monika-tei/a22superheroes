d5361644259bacf657b8fbd5a523e0c9e91bae60
lacj@kea.dk <lacj@kea.dk>
2022-08-26 09:08:22 +0200

    init

---
+++ superheroes.js
@@ -0,0 +1,8 @@
+const superheroes = [{}];
+
+alert(
+  "1. Checking if JS file is linked up correctly. \n2. Always open the inspector/console when woorking with the JS \n3. Write your code in the end of the js-file \n4. Don't use alert boxes for UI... \n5. Delete this message or comment it out"
+);
+console.table(superheroes);
+
+/* write your code here */
