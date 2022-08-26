cacdf279fc5bb5518edee0e9c00f8e87f85cc92d
lacj@kea.dk <lacj@kea.dk>
2022-08-26 11:39:57 +0200

    readyish, some imgs maybe broken, fix rest urself

--- superheroes.js
+++ superheroes.js
@@ -487,9 +487,9 @@ const superheroes = [
   },
 ];
 
-/* alert(
-  "1. Checking if JS file is linked up correctly. \n2. Always open the inspector/console when woorking with the JS \n3. Write your code in the end of the js-file \n4. Don't use alert boxes for UI... \n5. Delete this message or comment it out"
-); */
+alert(
+  "1. Checking if JS file is linked up correctly. \n2. Always open the inspector/console when woorking with the JS \n3. Write your code in the end of the js-file \n4. Don't use alert boxes for UI... \n5. Comment this line out - using /* */"
+);
 console.table(superheroes);
 
 /* write your code here */
